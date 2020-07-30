5c2f98f5d19954222c6c27cbb4730b31e1bb191f
Stan le Punk <stanislasiii@gmail.com>
2020-07-30 16:05:07 +0200

    juste une mise en place c'est chaud

---
+++ src/rope.hpp
@@ -0,0 +1,11 @@
+#ifndef ROPE_H
+# define ROPE_H
+#pragma once
+
+#include "../rope/rope.hpp"
+#include "../rope/rope_functions.hpp"
+#include "../rope/template/vec/vec3.hpp"
+#include "../rope/template/math/r_geom.hpp"
+#include <iostream>
+
+#endif
